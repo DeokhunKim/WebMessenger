2d93de7004cc46c7e8c1763483bf4c6315a4c072
DeokhunKim <86358502+DeokhunKim@users.noreply.github.com>
2022-05-11 17:32:26 +0900

    Add files via upload

---
+++ src/main/java/com/example/WebMessenger/WebMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebMessenger/WebMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
